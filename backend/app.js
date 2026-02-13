3b13d395e287262c74d829865a41cc86a9e10a7d
tanislf <tanialopezmoretz@gmail.com>
2026-02-12 23:05:30 -0600

    new vercel 8

--- backend/app.js
+++ backend/app.js
@@ -9,16 +9,7 @@ import errorHandler from "./middlewares/errorHandler.js";
 const app = express();
 
 //cors
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
-
-app.options("*", cors());
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoute);
