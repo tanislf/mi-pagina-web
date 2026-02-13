0a67a3eedb90b7d9d0f01bc16dafe231e4b29b22
tanislf <tanialopezmoretz@gmail.com>
2026-02-12 22:13:50 -0600

    new vercel 5

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import portfolioRoute from "./routes/portfolio.routes.js";
 import authRoute from "./routes/auth.routes.js";
 import contactRoute from "./routes/contact.routes.js";
@@ -7,44 +8,45 @@ import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
 
-// Orígenes permitidos
-const allowedCors = [
-  "https://tripleten.tk",
-  "http://tripleten.tk",
-  "http://localhost:3000",
-  "http://localhost:3001",
-  "http://localhost:5173",
-  "http://127.0.0.1:5173",
-  "https://tanislf.github.io",
-  "https://tanialopezmoretz.github.io",
-];
+// // Orígenes permitidos
+// const allowedCors = [
+//   "https://tripleten.tk",
+//   "http://tripleten.tk",
+//   "http://localhost:3000",
+//   "http://localhost:3001",
+//   "http://localhost:5173",
+//   "http://127.0.0.1:5173",
+//   "https://tanislf.github.io",
+//   "https://tanialopezmoretz.github.io",
+// ];
 
-app.use(function (req, res, next) {
-  const { origin } = req.headers;
+// app.use(function (req, res, next) {
+//   const { origin } = req.headers;
 
-  // Verificar si el origen está en la lista permitida
-  if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
+//   // Verificar si el origen está en la lista permitida
+//   if (allowedCors.includes(origin)) {
+//     res.header("Access-Control-Allow-Origin", origin);
+//   }
 
-  // Permitir métodos HTTP necesarios
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+//   // Permitir métodos HTTP necesarios
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
 
-  // Permitir encabezados necesarios
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-  );
+//   // Permitir encabezados necesarios
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+//   );
 
-  // Manejar solicitudes preflight OPTIONS
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-    return;
-  }
+//   // Manejar solicitudes preflight OPTIONS
+//   if (req.method === "OPTIONS") {
+//     res.sendStatus(200);
+//     return;
+//   }
 
-  next();
-});
+//   next();
+// });
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoute);
