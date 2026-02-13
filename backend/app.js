e738f8698f46542ef2387d865212f4eaaa4484d2
tanislf <tanialopezmoretz@gmail.com>
2026-02-12 22:32:33 -0600

    new vercel 6

--- backend/app.js
+++ backend/app.js
@@ -8,44 +8,7 @@ import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
 
-// // Orígenes permitidos
-// const allowedCors = [
-//   "https://tripleten.tk",
-//   "http://tripleten.tk",
-//   "http://localhost:3000",
-//   "http://localhost:3001",
-//   "http://localhost:5173",
-//   "http://127.0.0.1:5173",
-//   "https://tanislf.github.io",
-//   "https://tanialopezmoretz.github.io",
-// ];
-
-// app.use(function (req, res, next) {
-//   const { origin } = req.headers;
-
-//   // Verificar si el origen está en la lista permitida
-//   if (allowedCors.includes(origin)) {
-//     res.header("Access-Control-Allow-Origin", origin);
-//   }
-
-//   // Permitir métodos HTTP necesarios
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-
-//   // Permitir encabezados necesarios
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-//   );
-
-//   // Manejar solicitudes preflight OPTIONS
-//   if (req.method === "OPTIONS") {
-//     res.sendStatus(200);
-//     return;
-//   }
-
-//   next();
-// });
-
+//cors
 app.use(cors());
 app.use(express.json());
 
